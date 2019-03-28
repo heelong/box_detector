a7956da0090079157c272f5d116a6a58bcaba5aa
hel <heelong@cug.edu.cn>
2019-03-28 23:05:58 +0800

    add random noise for each point

--- src/point_cloud_publish.cpp
+++ src/point_cloud_publish.cpp
@@ -16,8 +16,8 @@ using namespace std;
 #define POINT_SIZE 2500
 #endif
 
-#ifndef BOX_SIZE
-#define BOX_SIZE 400
+#ifndef BOX_SIZE_ONE_PLANE
+#define BOX_SIZE_ONE_PLANE 100
 #endif
 
 #ifndef PI
@@ -40,10 +40,14 @@ bool judge_moveable(const float&x, const float&y, const float&x_max, const float
   else
     return false;
 }
-
+bool addNoise(pcl::PointXYZ& basic_point){
+	basic_point.x+=(0.0002* rand() / (RAND_MAX + 1.0f) - 0.0001f);
+	basic_point.y+=(0.0002* rand() / (RAND_MAX + 1.0f) - 0.0001f);
+	basic_point.z+=(0.0002* rand() / (RAND_MAX + 1.0f) - 0.0001f);
+}
 void move_center(){
 
-  const float move_step = 0.1f;
+  const float move_step = 0.01f;
 
   float theta_tmp = theta;
   float center_x_tmp = center_x + move_step*cos(theta);
@@ -64,22 +68,81 @@ void make_clouds(pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr) {
   double sigma_plane_z = 0.2;
   double sigma_point[3] = {0.2,0.2,0.2};
   pcl::PointXYZ basic_point;
-  int total_point = POINT_SIZE + BOX_SIZE;
+  int total_point = POINT_SIZE + BOX_SIZE_ONE_PLANE*5;
   basic_cloud_ptr->points.resize(total_point);
   for (size_t i = 0; i < POINT_SIZE; ++i)
   {
     basic_cloud_ptr->points[i].x = 10.0f * rand() / (RAND_MAX + 1.0f) - 5.0f;
     basic_cloud_ptr->points[i].y = 10.0f * rand() / (RAND_MAX + 1.0f) - 5.0f;
     basic_cloud_ptr->points[i].z = 0.0f;
+    addNoise(basic_cloud_ptr->points[i]);
   }
   move_center();
-  for (size_t i = POINT_SIZE; i < total_point; ++i)
-  {
-    basic_cloud_ptr->points[i].x = 1.0f * rand() / (RAND_MAX + 1.0f) - 0.5f+center_x;
-    basic_cloud_ptr->points[i].y = 1.0f * rand() / (RAND_MAX + 1.0f) - 0.5f+center_y;
-    basic_cloud_ptr->points[i].z = 1.0f * rand() / (RAND_MAX + 1.0f);
-  }
 
+  float cos_theta=cos(theta);
+  float sin_theta=sin(theta);
+
+  int count_1=POINT_SIZE;
+  int count_2=POINT_SIZE+BOX_SIZE_ONE_PLANE;
+  //plane 1;
+  for (size_t i = count_1; i < count_2; ++i)
+  {
+
+    basic_point.x=0.5f;
+    basic_point.y=1.0f * rand() / (RAND_MAX + 1.0f) - 0.5f;
+    basic_cloud_ptr->points[i].x = basic_point.x*cos_theta+basic_point.y*sin_theta+center_x;
+    basic_cloud_ptr->points[i].y = basic_point.y*cos_theta-basic_point.x*sin_theta+center_y;
+    basic_cloud_ptr->points[i].z = 1.0f * rand() / (RAND_MAX + 1.0f);
+    addNoise(basic_cloud_ptr->points[i]); 
+  }
+  count_1=count_2;
+  count_2=count_1+BOX_SIZE_ONE_PLANE;
+  //plane 2;
+  for (size_t i = count_1; i < count_2; ++i)
+  {
+    basic_point.x = 1.0f * rand() / (RAND_MAX + 1.0f) - 0.5f;
+    basic_point.y = 0.5f;
+    basic_cloud_ptr->points[i].x = basic_point.x*cos_theta+basic_point.y*sin_theta+center_x;
+    basic_cloud_ptr->points[i].y = basic_point.y*cos_theta-basic_point.x*sin_theta+center_y;
+    basic_cloud_ptr->points[i].z = 1.0f * rand() / (RAND_MAX + 1.0f);
+    addNoise(basic_cloud_ptr->points[i]);
+  }
+  count_1=count_2;
+  count_2=count_1+BOX_SIZE_ONE_PLANE;
+  //plane 3;
+  for (size_t i = count_1; i < count_2; ++i)
+  {
+    basic_point.x = -0.5f;
+    basic_point.y = 1.0f * rand() / (RAND_MAX + 1.0f) - 0.5f;
+    basic_cloud_ptr->points[i].x = basic_point.x*cos_theta+basic_point.y*sin_theta+center_x;
+    basic_cloud_ptr->points[i].y = basic_point.y*cos_theta-basic_point.x*sin_theta+center_y;
+    basic_cloud_ptr->points[i].z = 1.0f * rand() / (RAND_MAX + 1.0f);
+    addNoise(basic_cloud_ptr->points[i]);
+  }
+  count_1=count_2;
+  count_2=count_1+BOX_SIZE_ONE_PLANE;
+  //plane 4;
+  for (size_t i = count_1; i < count_2; ++i)
+  {
+    basic_point.x = 1.0f * rand() / (RAND_MAX + 1.0f) - 0.5f;
+    basic_point.y = -0.5f;
+    basic_cloud_ptr->points[i].x = basic_point.x*cos_theta+basic_point.y*sin_theta+center_x;
+    basic_cloud_ptr->points[i].y = basic_point.y*cos_theta-basic_point.x*sin_theta+center_y;
+    basic_cloud_ptr->points[i].z = 1.0f * rand() / (RAND_MAX + 1.0f);
+    addNoise(basic_cloud_ptr->points[i]);
+  }
+  count_1=count_2;
+  count_2=count_1+BOX_SIZE_ONE_PLANE;
+  //plane 5;
+  for (size_t i = count_1; i < count_2; ++i)
+  {
+    basic_point.x = 1.0f * rand() / (RAND_MAX + 1.0f) - 0.5f;
+    basic_point.y = 1.0f * rand() / (RAND_MAX + 1.0f) - 0.5f;
+    basic_cloud_ptr->points[i].x = basic_point.x*cos_theta+basic_point.y*sin_theta+center_x;
+    basic_cloud_ptr->points[i].y = basic_point.y*cos_theta-basic_point.x*sin_theta+center_y;
+    basic_cloud_ptr->points[i].z = 1.0f;
+    addNoise(basic_cloud_ptr->points[i]);
+  }
   basic_cloud_ptr->width = (int)basic_cloud_ptr->points.size();
   basic_cloud_ptr->height = 1;
   basic_cloud_ptr->header.frame_id = "point_cloud";
